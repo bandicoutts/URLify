b2d1eac24b86595290d017baf93e7c125104b30f
bandicoutts <14368864+bandicoutts@users.noreply.github.com>
2019-02-04 20:38:58 +1300

    Initial commit

---
+++ URLify.java
@@ -0,0 +1,34 @@
+/*
+
+Written by David Coutts
+4.2.2019
+
+Write a method to replace all spaces in a string with '%20:
+You may assume that the string has sufficient space at the end to hold the additional characters,
+and that you are given the "true" length of the string.
+(Note: If implementing in Java, please use a character array so that you can perform this operation in place.)
+
+EXAMPLE:
+
+Input: "Mr John Smith " J 13 Output: "Mr%20J ohn%20Smith" Hints: #53, #7 78
+
+ */
+
+
+import java.util.Scanner;
+
+
+public class URLify {
+
+    public static String getInput(){
+        Scanner reader = new Scanner(System.in);
+        String result;
+        result = reader.nextLine();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getInput());
+
+    }
+}
